61abe1801897dc26876560d1862812068df003f2
Saahil <neon@saahild.com>
2024-12-24 02:28:19 -0500

    chore: zeon:ai_comment

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
-let repo_url, demo_url
+let repo_url, demo_url, matchup_title, matchup_author
+let do_not_event_bother_running = false;
 
 async function main() {
+    if(do_not_event_bother_running) return;
     const textEl = document.querySelector("#voting-reason-container")
     if(!textEl) return;
     if(textEl.getAttribute("data-touched")) return;
@@ -15,13 +17,17 @@ async function main() {
     let button = document.querySelector("#submit-vote")
     button.addEventListener('click', async (e) => {
         e.preventDefault()
+        console.log(chrome.storage)
         console.debug(`button`)
         // send a req to webhook.site for now cuz its dev
        // find the matchup thats being voted for??
        const matchupRepoUrl =repo_url
         const matchupDemoUrl =  demo_url
+        const anon = await chrome.storage.sync.get("anon").then(d=>d.anon) || false
+        const send_it_to_user = await chrome.storage.sync.get("send_to_user").then(d=>d.send_to_user) || false
         console.debug(`matchupRepoUrl`, matchupRepoUrl)
         console.debug(`matchupDemoUrl`, matchupDemoUrl)
+        console.log(matchup_author, matchup_title)
        await fetch("https://api.saahild.com/api/highseasships/send_vote", {
             method: "POST",
             headers: {
@@ -31,14 +37,18 @@ async function main() {
             body: JSON.stringify({
                 
             vote: word,
-            send_it_to_user: false, // for now
-            anon: true, // for now
+            send_it_to_user, // for now
+            anon, 
             repo: matchupRepoUrl,
-            demo: matchupDemoUrl
+            demo: matchupDemoUrl,
+            title: matchup_title,
+            author: matchup_author
             })
         })
         // will have to rebind somehow
+        // future neon: wdym rebind
     })
+
 }
 setInterval(() => {
 if(window.location.pathname.includes("/wonderdome")) {
@@ -47,6 +57,8 @@ if(window.location.pathname.includes("/wonderdome")) {
         d.addEventListener('click', async (e) => {
             repo_url = d.parentElement.parentElement.parentElement.querySelector("#repository-link").href
             demo_url = d.parentElement.parentElement.parentElement.querySelector("#live-demo-link").href
+            matchup_title = d.parentElement.parentElement.parentElement.querySelector(`h2[class="font-heading text-2xl font-semibold text-indigo-600 dark:text-indigo-300 mb-4`).innerText.replace(`\nThis is a project update`, ``).trim()
+            matchup_author = d.parentElement.parentElement.parentElement.querySelector(`span[class="inline-flex items-center gap-1 rounded-full px-2 border text-sm leading-none text-purple-600 bg-purple-50 border-purple-500/10 "`).parentElement.href.split("?channel=")[1]
             console.debug(`repo_url`, repo_url)
             console.debug(`demo_url`, demo_url)
         })
@@ -56,3 +68,10 @@ if(window.location.pathname.includes("/wonderdome")) {
 } else {
 }
 }, 150)
+window.onload = async () => {
+    const isAuthed = await chrome.storage.sync.get('is_authed').then(d=>d.is_authed)    
+    if(!isAuthed) {
+        alert(`You are not authed! please go to the options page for the chrome extension "High Seas Share Votes"`)
+    do_not_event_bother_running = true;
+    }
+}
---
+++ options.js
@@ -0,0 +1,33 @@
+;(async () => {
+    const anon = document.getElementById("anon")
+const send_to_user = document.getElementById("sent_to_user")
+anon.addEventListener("change", (e) => {
+    // save via chrome 
+    chrome.storage.sync.set({anon: e.target.checked})
+  })
+  send_to_user.addEventListener("change", (e) => {
+      chrome.storage.sync.set({send_to_user: e.target.checked})
+  })
+  console.log(await chrome.storage.sync.get("anon"))
+anon.checked = await chrome.storage.sync.get("anon").then(d=>d.anon) || false
+send_to_user.checked = await chrome.storage.sync.get("send_to_user").then(d=>d.send_to_user) || false
+if(!await chrome.storage.sync.get(`is_authed`).then(d=>d.is_authed)) {
+    const h1 = document.querySelector("h1")
+    h1.innerText =`Authorize with slack to share your votes!`
+    const a = document.createElement("a")
+    a.target = "_blank"
+    a.href = "https://api.saahild.com/api/highseasships/slack/oauth"
+    a.innerText = "Authorize, then "
+    const btn = document.createElement("button")
+    btn.innerText = "click here once you have to get rid of this popup"
+    btn.addEventListener("click", async (e) => {
+        chrome.storage.sync.set({is_authed: true})
+        document.querySelector("#noauth").remove()
+    })
+
+    document.querySelector("#noauth").appendChild(h1)
+    document.querySelector("#noauth").appendChild(a)
+    document.querySelector("#noauth").appendChild(btn)
+}
+
+})()
